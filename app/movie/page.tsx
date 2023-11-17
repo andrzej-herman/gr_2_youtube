d9a5fed8d49f39b7215f53118e77248c406259c6
Andrzej Herman <andrzejek.herman@gmail.com>
2023-11-17 12:54:43 +0100

    Add project

---
+++ app/movie/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Movie() {
+    return (
+        <div>
+            <h6>Movie Site !!!</h6>
+        </div>
+    )
+}
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className="h-full  sflex items-center justify-center">
+      <h6 className="text-4xl text-green-600 font-bold tracking-tight">Welcome to YouTube Clone !!!</h6>
+    </div>
+  )
+}
